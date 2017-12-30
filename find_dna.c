074395390f27736dd910dc4813f7890c8f3dee4f
amichaiman <32230393+amichaiman@users.noreply.github.com>
2017-12-30 14:52:32 +0200

    Update find_dna.c

--- find_dna.c
+++ find_dna.c
@@ -16,62 +16,65 @@ void remove_enter(char *str);			//removes enter char ('\n')
 
 int main(int argc, char **argv)
 {
-		int i = 0;
-        int num_of_seq = 0;
-        FILE *fp;
-		int seq_num = 1;
-		
-		int seq_loc;
-        int gene_beg_loc;
-        int gene_end_loc;
-		
-		char seq_name[MAX_SEQ_NAME+1];	
-		char end_of_seq_name;
-		
-        num_of_seq = atoi(argv[2]);
-
-        if ((fp = fopen(argv[1],"r")) == NULL || num_of_seq < MIN_NUM_OF_SEQ || num_of_seq > MAX_NUM_OF_SEQ )
-        {
-                printf("ERROR: Invalid input\n");
-                exit(1);
-        }
-		
+	int i = 0;
+	int num_of_seq = 0;
+	FILE *fp;
+	int seq_num = 1;
 	
-		fgets(seq_name,MAX_SEQ_NAME,fp);	//first seq name
-		remove_enter(seq_name);			
-		seq_loc = ftell(fp);				//location of beginning of current sequence
-		
-		
-		printf("Sequence\tGene_size\tStart\t\tStart_codon\tEnd\t\tEnd_codon\n");
-		while(seq_num <= num_of_seq)
+	int seq_loc;
+	int gene_beg_loc;
+	int gene_end_loc;
+	
+	char seq_name[MAX_SEQ_NAME+1];	
+	char end_of_seq_name;
+	
+	num_of_seq = atoi(argv[2]);
+
+	if ((fp = fopen(argv[1],"r")) == NULL || num_of_seq < MIN_NUM_OF_SEQ || num_of_seq > MAX_NUM_OF_SEQ )
+	{
+		printf("ERROR: Invalid input\n");
+		exit(1);
+	}
+	
+
+	fgets(seq_name,MAX_SEQ_NAME,fp);	//first seq name
+	remove_enter(seq_name);			
+	seq_loc = ftell(fp);				//location of beginning of current sequence
+	
+	
+	printf("Sequence\tGene_size\tStart\t\tStart_codon\tEnd\t\tEnd_codon\n");
+	while(seq_num <= num_of_seq)
+	{
+		gene_beg_loc = find_sub_str(fp,START_CODON);
+
+		if (gene_beg_loc == -1)
 		{
-			gene_beg_loc = find_sub_str(fp,START_CODON);
+			fgets(seq_name,MAX_SEQ_NAME,fp);
 			
-			if (gene_beg_loc == -1)
-			{
-				fgets(seq_name,MAX_SEQ_NAME,fp);
-				remove_enter(seq_name);
-				seq_loc = ftell(fp);
-				seq_num++;
-			}
-			else
-			{
-				gene_beg_loc -= CODON_LEN;
-				gene_end_loc = find_sub_str(fp,END_CODON)-CODON_LEN+1;
-				
-				printf("%s\t\t",seq_name);
-				printf("%d\t\t",gene_end_loc-gene_beg_loc+CODON_LEN);
-				printf("%d\t\t",gene_beg_loc-seq_loc);
-				printf("%s\t\t",START_CODON);
-				printf("%d\t\t",gene_end_loc-seq_loc);
-				printf("%s\n",END_CODON);
-			}
+			if (feof(fp))
+				break;
 			
+			remove_enter(seq_name);
+			seq_loc = ftell(fp);
+			seq_num++;
+		}
+		else
+		{
+			gene_beg_loc -= CODON_LEN;
+			gene_end_loc = find_sub_str(fp,END_CODON)-CODON_LEN+1;
+			
+			printf("%s\t\t",seq_name);
+			printf("%d\t\t",gene_end_loc-gene_beg_loc+CODON_LEN);
+			printf("%d\t\t",gene_beg_loc-seq_loc);
+			printf("%s\t\t",START_CODON);
+			printf("%d\t\t",gene_end_loc-seq_loc);
+			printf("%s\n",END_CODON);
 		}
 		
-		
-		
-        return 0;
+	}
+	
+	fclose(fp);
+	return 0;
 }
 
 int find_sub_str(FILE *fp, char *sub_str)
@@ -83,13 +86,13 @@ int find_sub_str(FILE *fp, char *sub_str)
 		return -1;
 	}
 	
-	while (strcmp(cur_codon, sub_str) != 0)	//check if i am the beggining of seq
+	while (strcmp(cur_codon, sub_str) != 0)	//check if i am the beginning of seq
 	{
 
 		fseek(fp,-2,SEEK_CUR);			//get next	
 		if (fgets(cur_codon,CODON_LEN+1,fp) == NULL)
 		{
-			exit(0);
+			return -1;
 		}
 		
 		if (strlen(cur_codon) < CODON_LEN)
@@ -112,5 +115,4 @@ void remove_enter(char *str)
 	
 	str[i] = '\0';
 }
-	
 	
